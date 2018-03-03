1dfc410f9b0f2a8e798a619503285adb3038cfa8
Lalit Adithya <lalitadithya5071@gmail.com>
2018-03-03 20:24:52 +0530

    Added interface for container manager

---
+++ Interfaces/IContainerManager.cs
@@ -0,0 +1,8 @@
+﻿namespace Interfaces
+{
+    interface IContainerManager
+    {
+        void InitClient(string url);
+        (string, string) RunImage(string image, string tag, string[] args);
+    }
+}
