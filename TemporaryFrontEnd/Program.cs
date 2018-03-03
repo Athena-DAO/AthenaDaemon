3c15ed3f1db38c1d93bf3dfba76ce9a69d11da29
Lalit Adithya <lalitadithya5071@gmail.com>
2018-03-03 21:09:48 +0530

    Updated frontend

--- TemporaryFrontEnd/Program.cs
+++ TemporaryFrontEnd/Program.cs
@@ -10,36 +10,43 @@ namespace TemporaryFrontEnd
 {
     class Program
     {
-        //private static DockerManager containerManager;
 
         static void Main(string[] args)
         {
-            //containerManager = DockerManager.Instance;
-            //containerManager.InitClient("npipe://./pipe/docker_engine");
-            //(string output, string error) = containerManager.RunImage("lalitadithya/sampleapp", "latest", new string[] { "-u", "url" });
-            //Console.WriteLine("output is " + output);
-            //Console.WriteLine("error is " + error);
-
+            int numberOfContainers = 2;
             Console.WriteLine("Number of containers = " + MachineInformation.NumberOfContainersSupported);
+            StartService(DockerManager.Instance, numberOfContainers);
             Console.ReadLine();
+        }
 
-            IListener listener = new Listener(new Consumer("localhost", "task_queue1"), new MessageRecieved());
-
-            new Thread(() =>
+        private static void StartService(IContainerManager containerManager, int numberOfContainers)
+        {
+            containerManager.InitClient("npipe://./pipe/docker_engine");
+            for (int i = 0; i < numberOfContainers; i++)
             {
-                listener.StartListening();
-            }).Start();
-
-            //Consumer myConsumer = new Consumer("localhost", "task_queue1");
-            //myConsumer.Consume(new MessageRecieved());
-            Console.ReadLine();
+                IListener listener = new Listener(new Consumer("localhost", "task_queue1"), new MessageRecieved(containerManager));
+                new Thread(() =>
+                {
+                    listener.StartListening();
+                }).Start();
+            }
         }
 
         private class MessageRecieved : IMessageRecieved
         {
+            IContainerManager containerManager;
+
+            public MessageRecieved(IContainerManager containerManager)
+            {
+                this.containerManager = containerManager;
+            }
+
             public void ProcessMessage(string message)
             {
-                Console.WriteLine("Recieved " + message);
+                Console.WriteLine(Thread.CurrentThread.Name + " Recieved " + message);
+                (string output, string error) = containerManager.RunImage("lalitadithya/sampleapp", "latest", new string[] { "-u", "url" });
+                Console.WriteLine(Thread.CurrentThread.Name + " output is " + output);
+                Console.WriteLine(Thread.CurrentThread.Name + " error is " + error);
             }
         }
     }
