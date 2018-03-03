11b56cc795aa26e8e8549d8f83570f3983f10ed9
Lalit Adithya <lalitadithya5071@gmail.com>
2018-03-03 19:39:48 +0530

    Removed deadcode

--- TemporaryFrontEnd/Program.cs
+++ TemporaryFrontEnd/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ContainerManagement;
-using ContainerWrapper;
 using MachineManagementAndInformation;
 using QueueConsumer;
 using System;
@@ -9,26 +8,27 @@ namespace TemporaryFrontEnd
 {
     class Program
     {
+        private static DockerManager containerManager;
+
         static void Main(string[] args)
         {
-            DockerManager containerManager = DockerManager.Instance;
+            containerManager = DockerManager.Instance;
             containerManager.InitDockerClient("npipe://./pipe/docker_engine");
             (string output, string error) = containerManager.RunImage("lalitadithya/sampleapp", "latest", new string[] { "-u", "url" });
             Console.WriteLine("output is " + output);
             Console.WriteLine("error is " + error);
 
             Console.WriteLine("Number of containers = " + MachineInformation.NumberOfContainersSupported);
-
             Console.ReadLine();
 
-            Consumer myConsumer = new Consumer("localhost", "task_queue1", MyCallback);
-            myConsumer.Consume("task_queue1");
-
-            Console.ReadLine();
+            //Consumer myConsumer = new Consumer("localhost", "task_queue1", MyCallback);
+            //myConsumer.Consume("task_queue1");
+            //Console.ReadLine();
         }
 
         private static void MyCallback(string message)
         {
+
             Console.WriteLine("Recieved " + message);
         }
     }
